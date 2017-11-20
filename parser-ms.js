93667c14a33da2fb4e56e88f586053368f860144
Nicholas Kolatsis <nkolatsis@users.noreply.github.com>
2017-11-20 20:48:35 +0100

    Fixed a bug that caused the incorrect IP to display.

--- parser-ms.js
+++ parser-ms.js
@@ -1,10 +1,11 @@
 var express = require("express")
+var requestIp = require("request-ip")
 
 var app = express()
 app.listen(5000)
 
 app.get("/", (req, res) => {
-    var ipaddress = req.connection.remoteAddress
+    var ipaddress = requestIp.getClientIp(req)
     var language = req.headers["accept-language"].split(",")[0]
     var software = req.headers["user-agent"].match(/^.*\((.*)\).*$/)[1]
     var userDetails = {"ipaddress": ipaddress, "language" : language, "software": software }
